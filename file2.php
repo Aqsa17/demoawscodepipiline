3dade68d0faac68cb124b5520d67c9f6a4f6a740
Aqsa shaikh <aqsa.shaikh@salesflo.com>
2023-11-06 15:19:47 +0500

    filepushedbyaqsashaikh

---
+++ file2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>this is our second file</title>
+</head>
+<body>
+    <h1>this is our second file pushed by our local system to AWS Code commmit .</h1>
+    
+</body>
+</html>
