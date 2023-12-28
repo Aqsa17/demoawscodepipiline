92d5abf399387b8b7cc58116444e391ed9d05eec
Aqsa17 <54582796+Aqsa17@users.noreply.github.com>
2023-12-28 12:43:21 +0500

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Congratulations your CICD successfully implemented :</h1>
-    <h2>Aqsa shaikh changes this files </h2>
+    <h2>Aqsa shaikh changes this files change extension in php</h2>
     
 </body>
 </html>
