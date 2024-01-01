5f9a21b87ff2d9e2b0f94e9ef2059461c7ae44fb
Aqsa shaikh <aqsa.shaikh@salesflo.com>
2024-01-01 17:15:30 +0500

    updatebuilspecfile

--- testingdemoweb/check_index_file.php
+++ testingdemoweb/check_index_file.php
@@ -1,6 +1,6 @@
 <?php
 error_log("Script execution started");
-$filePath = __DIR__ . '/services.php';
+$filePath = __DIR__ . '/service.php';
 echo  $filePath;
 if (file_exists($filePath)) {
     echo "service.php file exists.";
