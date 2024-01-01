117dad2d83909f9287a44044cce19c4a9b47973d
Aqsa shaikh <aqsa.shaikh@salesflo.com>
2024-01-01 17:37:10 +0500

    update url in index file

--- testingdemoweb/index.php
+++ testingdemoweb/index.php
@@ -69,19 +69,19 @@ else{
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav  ">
               <li class="nav-item active">
-                <a class="nav-link" href="/testingdemoweb/index.php">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/testingdemoweb/about.php"> About</a>
+                <a class="nav-link" href="about.php"> About</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/testingdemoweb/service.php">Services</a>
+                <a class="nav-link" href="service.php">Services</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/testingdemoweb/why.php">Why Us</a>
+                <a class="nav-link" href="why.php">Why Us</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/testingdemoweb/team.php">Team</a>
+                <a class="nav-link" href="team.php">Team</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#"> <i class="fa fa-user" aria-hidden="true"></i> Login</a>
