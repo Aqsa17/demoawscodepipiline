b4fdbff5ab483fdf60fc99d9c238da298a0f6816
Aqsa shaikh <aqsa.shaikh@salesflo.com>
2024-01-01 12:32:33 +0500

    updatebuildfile

--- check_index_file.php
+++ check_index_file.php
@@ -1,5 +1,6 @@
 <?php
-$filePath = __DIR__ . 'testingdemoweb/index.php';
+error_log("Script execution started");
+$filePath = __DIR__ . '/check_index_file.php';
 echo  $filePath;
 if (file_exists($filePath)) {
     echo "index.php file exists.";
